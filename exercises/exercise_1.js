2a1bb046c8f18d3fc0e62c3d887520cdb91539c8
Golden-I <65423579+Golden-I@users.noreply.github.com>
2022-09-27 21:59:36 +0300

    Update exercise_1.js
    feature for exercise 1

--- exercises/exercise_1.js
+++ exercises/exercise_1.js
@@ -3,7 +3,10 @@ const characters = require("../data/characters.json");
 /* TODO: return one character with an "unknown" birth year  */
 
 const findCharacterUnknownBirthDate = () => {
+  // find.characters;
+  // return (birth_year = "unknown");
   // CODE HERE
+  return characters.find((character) => character.birth_year === "unknown");
 };
 
 module.exports = findCharacterUnknownBirthDate;
